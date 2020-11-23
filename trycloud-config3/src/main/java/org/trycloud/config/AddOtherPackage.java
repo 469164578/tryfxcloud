e636cf93cfe378e6ac8846e9de614179aaadf742
try <469164578@qq.com>
2020-11-23 20:55:33 +0800

    spring cloud的 config 高可用

---
+++ trycloud-config3/src/main/java/org/trycloud/config/AddOtherPackage.java
@@ -0,0 +1,10 @@
+package org.trycloud.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= {"org.fxytry.config"})
+public class AddOtherPackage {
+
+}
