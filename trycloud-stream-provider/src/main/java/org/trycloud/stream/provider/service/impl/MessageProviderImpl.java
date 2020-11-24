306808d8a9ce227999fe80413fc5d913bc423d41
try <469164578@qq.com>
2020-11-25 06:29:02 +0800

    SpringCloudStream添加

--- trycloud-stream-provider/src/main/java/org/trycloud/stream/provider/service/impl/MessageProviderImpl.java
+++ trycloud-stream-provider/src/main/java/org/trycloud/stream/provider/service/impl/MessageProviderImpl.java
@@ -2,17 +2,19 @@ package org.trycloud.stream.provider.service.impl;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
+import org.trycloud.channel.DefaultProcess;
 import org.trycloud.entity.Product;
 import org.trycloud.stream.provider.service.MessageProvider;
-@EnableBinding(Source.class)
+@EnableBinding(DefaultProcess.class)
 public class MessageProviderImpl implements MessageProvider {
 	@Resource
+    @Qualifier("try_output")
     private MessageChannel output;  // 消息的发送管道
-	@Override
+	@Override	
 	public void send(Product product) {
 		output.send(MessageBuilder.withPayload(product).build());
 	}
